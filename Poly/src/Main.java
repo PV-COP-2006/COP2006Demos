69bb13a85f64b41486f65dae6a20d4863bb8184e
svanselow <svanselow@L438PDPC.STUDENT.ad.fgcu.edu>
2017-04-13 18:19:20 -0400

    Mod 13

--- Poly/src/Main.java
+++ Poly/src/Main.java
@@ -4,15 +4,13 @@ class Main
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Module1.do1();
-		Module2.do2();
-		Module3.do3();
-		Module4.do4();
-		Module5.do5();
-		Module11.do11();
-		
-		
-		
+		//Module1.do1();
+		//Module2.do2();
+		//Module3.do3();
+		//Module4.do4();
+		//Module5.do5();
+		//Module11.do11();
+		Module13.do13();
 	}
 	
 }
