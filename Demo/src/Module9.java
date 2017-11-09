570f5783460164dbc906475f102a32d50abf7581
svanselow <svanselow@H339PDPC.STUDENT.ad.fgcu.edu>
2017-11-09 09:31:09 -0500

    more strings

--- Demo/src/Module9.java
+++ Demo/src/Module9.java
@@ -6,9 +6,9 @@ public class Module9 {
     Scanner scanner = new Scanner(System.in);
     
     comparingStringsDemo(scanner);
-    stringsAreArraysOfCharacters(scanner);
-    stringsAreImmutable(scanner);
-    
+    stringsAreArraysOfCharacters();
+    stringsAreImmutable();
+    stringInitialValue();
     
     scanner.close();
   }
@@ -23,38 +23,62 @@ public class Module9 {
     
     System.out.println("With user input and ==");
     if (answer.toLowerCase() == "a") {
-      System.out.println("You entered a");
+      System.out.println("== a is true");
     } else {
-      System.out.println("You did not enter a");
+      System.out.println("== a is false");
     }
     
     System.out.println("With user input and .equals");
     if (answer.equals("a")) {
-      System.out.println("You entered a");
+      System.out.println(".equals a is true");
     } else {
-      System.out.println("You did not enter a");
+      System.out.println(".equals a is false");
     }
+    
+    answer = "a";
     
     System.out.println("With assignment and ==");
     if (answer == "a") {
-      System.out.println("You entered a");
+      System.out.println("== a is true");
     } else {
-      System.out.println("You did not enter a");
+      System.out.println("== a is false");
     }
     
     System.out.println("With assignment and .equals");
     if (answer.equals("a")) {
-      System.out.println("You entered a");
+      System.out.println(".equals a is true");
     } else {
-      System.out.println("You did not enter a");
+      System.out.println(".equals a is false");
     }
   }
 
-  public static void stringsAreArraysOfCharacters(Scanner scanner) {
+  public static void stringsAreArraysOfCharacters() {
     // Strings are a sequence of characters (like an array). 
-
+    String str = "I am really an array of characters.";
+    //System.out.println(str[0]);
+    System.out.println(str.charAt(0));
+    char[] characters = str.toCharArray();
+    characters[0] = 'z';
+    System.out.println(characters);
   }
   
-  public static void stringsAreImmutable(Scanner scanner) {
+  public static void stringsAreImmutable() {
+    String str = "I am immutable.";
+    str = "different string";
+    // Cannot be overwritten
+    //str.charAt(0) = "A"; 
   }
+  
+  public static void stringInitialValue() {
+    String str = new String();
+    int num1 = 0; 
+    if (str.isEmpty()) {
+      System.out.println("Empty");
+    }
+    else {
+      System.out.println(str);
+    }
+    System.out.println("here");
+  }
+  
 }
