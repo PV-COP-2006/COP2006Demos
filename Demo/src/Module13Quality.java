84f734ddf5a07904a7572f6fa361725ba1e6dfac
Scott <Scott@Alpha-6LXY322>
2017-12-29 11:40:56 -0500

    Added Javadoc comments.

--- Demo/src/Module13Quality.java
+++ Demo/src/Module13Quality.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 /**
+ * This is a class to demonstrate enhancing program quality.
  * @author Scott
  *
  */
 public class Module13Quality {
 
   /**
+   * Every method should have a Javadoc comment.
    * @param scan The scanner object for keyboard input. 
    */
   public static void do13(Scanner scan) {
