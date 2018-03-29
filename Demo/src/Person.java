e661c4b3bb73a1249db6a0af0af9770271dc7976
Scott Vanselow <profvanselow@gmail.com>
2018-03-29 18:30:59 -0400

    Interface

---
+++ Demo/src/Person.java
@@ -0,0 +1,19 @@
+
+public class Person {
+
+  protected int age;
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+  
+  public Person(int a) {
+    age = a;
+  }
+  
+
+}
---
+++ Demo/src/Student.java
@@ -0,0 +1,12 @@
+
+public class Student extends Person {
+
+  public Student() {
+    super(0);
+  }
+  
+  public final int getAge() {
+    return age;
+  }
+  
+}
