a3e10590e524e90eff6c558824a710965c5b5695
Scott Vanselow <profvanselow@gmail.com>
2018-09-20 08:56:42 -0400

    Updated 05

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -5,19 +5,19 @@
 class Main {
 
   public static void main(String[] args) throws java.lang.Exception {
-    Module01Hello.do1(); // Hello World
-    Module02Elements.do2(); // Classes, Data Types, Input, Variables, Strings basics
-    Module03Strings.do3(); // Classes and Methods
-    Module04Methods.do4(); // Classes and Objects
-    Module06Conditions.do6(); // Operators, Condition Statements and Branching
-    Module07Looping.do7(); // Loops
-    Module08OOP.do8(); // Dictionaries, Maps, Recursion
-    Module09OOP2.do9(); // More Strings
-    Module10Arrays.do10(); // 2d Arrays, ArrayLists, Generics, & Linked Lists
-    Module11Exceptions.do11(); // Inheritance and Polymorphism
-    Module13Quality.do13(); // Stacks
-    // LinkedList.doExercise();
-
+    //Module01Hello.do1(); // Hello World
+    //Module02Elements.do2(); // Classes, Data Types, Variables
+    //Module03Strings.do3(); // Strings, Casting, Input
+    Module04Methods.do4(); // Classes and Methods
+    //Module05Classes.do5(); // Library Classes
+    //Module06Conditions.do6(); // Operators, Condition Statements and Branching
+    //Module07Looping.do7(); // Loops
+    //Module08OOP.do8(); // 
+    //Module09OOP2.do9(); // Inheritance and Polymorphism
+    //Module10Arrays.do10(); // 
+    //Module11Exceptions.do11(); // 
+    //Module13Quality.do13(); // 
+    //LinkedList.doExercise();
 
   }
 
