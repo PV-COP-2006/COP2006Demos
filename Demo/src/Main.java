fea7584d40f419c5dde908aacd481f29780e9153
sdvansel <sdvansel@7127PDPC.STUDENT.ad.fgcu.edu>
2018-10-16 08:39:03 -0400

    Added to Loops

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -5,18 +5,8 @@ import java.util.Scanner;
  *
  */
 class Main {
-
   public static void main(String[] args) throws java.lang.Exception {
-    
-    Playerr player1 = new Playerr();
-    player1.setName("Mario");
-    System.out.println(player1.getName());
-    
-    int selection;
-    
-    // Create a scanner
-    Scanner scan = new Scanner(System.in);
-    
+
     // Display menu
     System.out.println("Enter a menu selection");
     System.out.println("1. Hello World");
@@ -31,52 +21,59 @@ class Main {
     System.out.println("10. ");
     System.out.println("11. ");
     System.out.println("13. Quality");
+    
+    
+    int selection;
+
+    // Create a scanner
+    Scanner scan = new Scanner(System.in);
+    
     selection = scan.nextInt();
-    
-    switch(selection) {
-    case 1:
-    	Module01Hello.do1(); // Hello World
-    	break;
-    case 2:
-    	Module02Elements.do2(); // Classes, Data Types, Variables
-    	break;
-    case 3:
-    	Module03Strings.do3(); // Strings, Casting, Input
-    	break;
-    case 4:
-    	Module04Methods.do4(); // Classes and Methods
-    	break;
-    case 5:
-    	Module05Classes.do5(); // Library Classes
-    	break;
-    case 6:
-    	Module06Conditions.do6(); // Operators, Condition Statements and Branching
-    	break;
-    case 7:
-    	Module07Looping.do7(); // Loops
-    	break;
-    case 8:
-    	Module08OOP.do8(); //
-    	break;
-    case 9:
-    	Module09OOP2.do9(); // Inheritance and Polymorphism
-    	break;
-    case 10:
-    	Module10Arrays.do10(); //
-    	break;
-    case 11:
-    	Module11Exceptions.do11(); //
-    	break;
-    case 13:
-    	Module13Quality.do13(); //
-    	break;
-    default:
-    	System.out.println("Invalid selection");
-    
+
+    switch (selection) {
+      case 1:
+        Module01Hello.do1(); // Hello World
+        break;
+      case 2:
+        Module02Elements.do2(); // Classes, Data Types, Variables
+        break;
+      case 3:
+        Module03Strings.do3(); // Strings, Casting, Input
+        break;
+      case 4:
+        Module04Methods.do4(); // Classes and Methods
+        break;
+      case 5:
+        Module05Classes.do5(); // Library Classes
+        break;
+      case 6:
+        Module06Conditions.do6(); // Operators, Condition Statements and Branching
+        break;
+      case 7:
+        Module07Looping.do7(); // Loops
+        break;
+      case 8:
+        Module08OOP.do8(); //
+        break;
+      case 9:
+        Module09OOP2.do9(); // Inheritance and Polymorphism
+        break;
+      case 10:
+        Module10Arrays.do10(); //
+        break;
+      case 11:
+        Module11Exceptions.do11(); //
+        break;
+      case 13:
+        Module13Quality.do13(); //
+        break;
+      default:
+        System.out.println("Invalid selection");
+
     }
 
-    // 
-    //LinkedList.doExercise();
+    //
+    // LinkedList.doExercise();
 
   }
 
--- Demo/src/Module07Looping.java
+++ Demo/src/Module07Looping.java
@@ -1,11 +1,34 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Module07Looping {
 
   public static void do7() {
     Scanner scanner = new Scanner(System.in);
+    
+    double principle = 3000;
+    double rate = 8;
+    double time = 1;
+    double interest;
+    final int TOTAL_YEARS = 40;
+    double monthlyIncrease = 450;
+    NumberFormat formatter = NumberFormat.getCurrencyInstance();
+    String interestString;
+    for (int year = 1; year <= TOTAL_YEARS; year++) {
+      /* Calculate compound interest */
+      interest = principle * (Math.pow((1 + rate / 100), time)) - principle; 
+      principle += interest + monthlyIncrease * 12;
+      interestString = formatter.format(interest);
+      System.out.println("Compound interest for year " + year + " is " + interestString);
+    }
+    String principleString;
+    principleString = formatter.format(principle);
+    System.out.println("The principle after " + TOTAL_YEARS + " years is " + principleString);
+    
+    // Demonstration of a while loop to repeat your program
     boolean continueProgram = true;
     int menuChoice = 0;
+    
     while (continueProgram) {
 
       // your program
